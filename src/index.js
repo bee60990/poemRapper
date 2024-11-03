698c447e29ab655170a3033bd0e60d21edeb5eef
bee60990 <155386705+bee60990@users.noreply.github.com>
2024-11-03 11:04:22 +0100

    Added API Call

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 function generatePoem(event) {
   event.preventDefault();
 
+  let apiKey = "9530ba64d5fta9c45b62cdbab1131eoe";
+  let context = "Generate a short, nice poem";
+  let prompt = document.querySelector("#instructions").value;
+  let apiCall = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiCall).then(displayPoem);
+}
+
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "Hello World",
+    strings: response.data.answer,
     autoStart: true,
     delay: 4,
     cursor: " ",
